b23e87ecd4ee4d3ddf4ce55a5a0074ca5c35b482
Dmitry Schegolikhin <d.schegolikhin@kt-team.de>
2018-07-30 23:01:40 +0400

    Show loader before button activation.

--- components/Button.vue
+++ components/Button.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="paypal-button"><span class="loader lds-dual-ring"/></div>
+  <div class="paypal-button"><span v-show="loader" class="loader lds-dual-ring"/></div>
 </template>
 
 <script>
@@ -17,6 +17,7 @@ export default {
   data () {
     const storeView = currentStoreView()
     return {
+      loader: false,
       commit: true,
       currency: storeView.i18n.currencyCode,
       locale: storeView.i18n.defaultLocale.replace('-', '_') // Convert to PayPal format of locale
@@ -40,24 +41,25 @@ export default {
       docHead.appendChild(docScript)
     },
     configurePaypal () {
-      this.$nextTick(() => {
-        window.paypal.Button.render({
-          // Pass in env
-          env: this.$config.paypal.env,
-          // Customize button (optional)
-          locale: this.locale, // Should be in format: 'en_US' accordance by PayPal Api (in VSF used 'en-US')
-          style: this.$config.paypal.style,
-          // Pass the payment details for your transaction
-          // See https://developer.paypal.com/docs/api/payments/#payment_create for the expected json parameters
-          payment: this.createPayment,
-          // Display a "Pay Now" button rather than a "Continue" button
-          commit: this.commit,
-          // Pass a function to be called when the customer completes the payment
-          onAuthorize: this.onAuthorize,
-          // Pass a function to be called when the customer cancels the payment
-          onCancel: this.onCancel
-        }, this.$el)
-      })
+      this.loader = true
+      window.paypal.Button.render({
+        // Pass in env
+        env: this.$config.paypal.env,
+        // Customize button (optional)
+        locale: this.locale, // Should be in format: 'en_US' accordance by PayPal Api (in VSF used 'en-US')
+        style: this.$config.paypal.style,
+        // Pass the payment details for your transaction
+        // See https://developer.paypal.com/docs/api/payments/#payment_create for the expected json parameters
+        payment: this.createPayment,
+        // Display a "Pay Now" button rather than a "Continue" button
+        commit: this.commit,
+        // Pass a function to be called when the customer completes the payment
+        onAuthorize: this.onAuthorize,
+        // Pass a function to be called when the customer cancels the payment
+        onCancel: this.onCancel
+      }, this.$el)
+
+      setTimeout(function () { this.loader = false }.bind(this), 2000)
     },
     getTransactions () {
       return [{ amount: { total: this.grandTotal, currency: this.currency } }]
@@ -125,6 +127,9 @@ export default {
   }
   .loader {
     position: absolute;
+    top: -12px;
+    left: 40px;
+    z-index: 200;
   }
   .lds-dual-ring {
     display: inline-block;
