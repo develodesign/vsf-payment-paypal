e2fba09a73421a370806fcd1d8a470828ddf8140
Dmitry Schegolikhin <d.schegolikhin@kt-team.de>
2018-12-04 00:18:40 +0400

    Moved load the paypal api external script to beforeRegistration hook.

--- hooks/beforeRegistration.ts
+++ hooks/beforeRegistration.ts
@@ -13,6 +13,13 @@ export function beforeRegistration(Vue, config, store, isServer) {
 
   if (!Vue.prototype.$isServer) {
 
+    let jsUrl = 'https://www.paypalobjects.com/api/checkout.js'
+    let docHead = document.getElementsByTagName('head')[0]
+    let docScript = document.createElement('script')
+    docScript.type = 'text/javascript'
+    docScript.src = jsUrl
+    docHead.appendChild(docScript)
+
     store.watch((state) => state.checkout.paymentDetails, (prevMethodCode, newMethodCode) => {
       if (newMethodCode === 'vsfpaypal') {
         // Register the handler for what happens when they click the place order button.
