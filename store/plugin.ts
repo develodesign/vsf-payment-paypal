f2cc330c1627e382df7332d3cc9fcab080fe1327
Max Malm <benjick@dumfan.net>
2019-06-11 22:13:08 +0200

    Remove boilerplate plugin

--- store/plugin.ts
+++
@@ -1,9 +0,0 @@
-import * as types from './mutation-types'
-
-export function plugin (mutation, state) {
-  if (types[mutation.type]) {
-    console.info('performed mutation from this store with type', mutation.type)
-  } else {
-    console.info('performed mutation from other store with type', mutation.type)
-  }
-}
