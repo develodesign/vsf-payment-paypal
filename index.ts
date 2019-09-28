d1f55a755c0ae965609f451471ed70527faa61c4
Dmitry Schegolikhin <d.shegolihin@gmail.com>
2019-09-28 20:00:23 +0400

    #107 Updated the module signatura - compatibility with VSF 1.10.x.

--- index.ts
+++ index.ts
@@ -1,13 +1,11 @@
+import { createModule } from '@vue-storefront/core/lib/module'
 import { module } from './store'
 import { beforeRegistration } from './hooks/beforeRegistration'
-import { VueStorefrontModule, VueStorefrontModuleConfig } from '@vue-storefront/core/lib/module'
 
 export const KEY = 'paypal'
 
-const moduleConfig: VueStorefrontModuleConfig = {
+export const Paypal = createModule({
   key: KEY,
   store: { modules: [{ key: KEY, module }]},
   beforeRegistration
-}
-
-export const Paypal = new VueStorefrontModule(moduleConfig)
+})
