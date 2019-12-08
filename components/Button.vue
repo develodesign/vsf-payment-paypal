d2197fe4ecd4efb663a30b2cd77789ad934eda7e
Dmitry Schegolikhin <d.shegolihin@gmail.com>
2019-12-08 20:23:25 +0400

    Added loader for the place order process. Backward compatibility for get totals.

--- components/Button.vue
+++ components/Button.vue
@@ -3,7 +3,7 @@
 </template>
 
 <script>
-import store from '@vue-storefront/core/store'
+import i18n from '@vue-storefront/i18n'
 import { currentStoreView } from '@vue-storefront/core/lib/multistore'
 
 export default {
@@ -15,6 +15,12 @@ export default {
       locale: storeView.i18n.defaultLocale.replace('-', '_') // Converting to PayPal format
     }
   },
+  beforeMount () {
+    this.$bus.$on('order-after-placed', this.onAfterPlaceOrder)
+  },
+  beforeDestroy () {
+    this.$bus.$off('order-after-placed', this.onAfterPlaceOrder)
+  },
   mounted () {
     window.paypal.Buttons({
       createOrder: this.createOrder,
@@ -23,11 +29,15 @@ export default {
   },
   computed: {
     grandTotal () {
-      let cartTotals = store.getters['cart/getTotals']
+      let cartTotals = this.$store.getters.hasOwnProperty('cart/totals') ? this.$store.getters['cart/totals'] : this.$store.getters['cart/getTotals']
       return cartTotals.find(seg => seg.code === 'grand_total').value
     }
   },
   methods: {
+    onAfterPlaceOrder () {
+      // Stop display loader
+      this.$bus.$emit('notification-progress-stop')
+    },
     getAmount () {
       return [{ amount: { value: this.grandTotal, currency_code: this.currencyCode } }]
     },
@@ -37,13 +47,16 @@ export default {
       })
     },
     async onApprove (data, actions) {
+      // Start display loader
+      this.$bus.$emit('notification-progress-start', [i18n.t('Placing Order'), '...'].join(''))
+
       const capture = await actions.order.capture()
 
       if (capture.status !== 'COMPLETED') {
         return false
       }
 
-      const completed = await store.dispatch('paypal/complete', { orderId: data.orderID })
+      const completed = await this.$store.dispatch('paypal/complete', { orderId: data.orderID })
 
       this.$bus.$emit('checkout-do-placeOrder', completed)
 
