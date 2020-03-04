f0aa612c26b17066bed1113bb8ca5c6a19198dbb
Philipp Sander <p.sander@netz98.de>
2020-03-04 17:30:44 +0100

    use base vsf-api URL from config

--- store/actions.ts
+++ store/actions.ts
@@ -2,11 +2,12 @@ import { PaypalState } from '../types/PaypalState'
 import { ActionTree } from 'vuex'
 import config from 'config'
 import { adjustMultistoreApiUrl } from '@vue-storefront/core/lib/multistore'
+import { processURLAddress } from '@vue-storefront/core/helpers'
 
 // it's a good practice for all actions to return Promises with effect of their execution
 export const actions: ActionTree<PaypalState, any> = {
   complete({ }, params) {
-    let url = config.paypal.endpoint.complete
+    let url = processURLAddress(config.paypal.endpoint.complete)
     url = config.storeViews.multistore ? adjustMultistoreApiUrl(url) : url
     return fetch(url, {
       method: 'POST',
@@ -19,7 +20,7 @@ export const actions: ActionTree<PaypalState, any> = {
     }).then(resp => { return resp.json() })
   },
   setExpressCheckout({ }, params) {
-    let url = config.paypal.endpoint.setExpressCheckout
+    let url = processURLAddress(config.paypal.endpoint.setExpressCheckout)
     url = config.storeViews.multistore ? adjustMultistoreApiUrl(url) : url
     return fetch(url, {
       method: 'POST',
