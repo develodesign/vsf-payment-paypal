5a2247cad4d984c5b43284da5db8d8941f65964f
Jamie <jamie@develodesign.co.uk>
2020-11-13 11:45:23 +0000

    Defer beforeRegistration Hook if config value `addPaypalJsToHead` is false + Documentation to add JS to head

--- hooks/beforeRegistration.ts
+++ hooks/beforeRegistration.ts
@@ -1,14 +1,13 @@
-import { isServer } from '@vue-storefront/core/helpers'
 import { currentStoreView } from '@vue-storefront/core/lib/multistore'
 
-export function beforeRegistration(config, store) {
+export function beforeRegistration({ Vue, config, store, isServer }) {
   const VSF_PAYPAL_CODE = 'paypal_express'
 
-  if (!isServer && config.hasOwnProperty('paypal')) {
+  if (!isServer && !config.paypal.addPaypalJsToHead) {
     const storeView = currentStoreView()
     const { currencyCode } = storeView.i18n
-    const clientId = config.paypal.hasOwnProperty('clientId') ? config.paypal.clientId : ''
-    const sdkUrl = `https://www.paypal.com/sdk/js?client-id=${clientId}&currency=${currencyCode}&disable-funding=card,credit,mybank,sofort`
+    const clientId = config.paypal.clientId
+    const sdkUrl = `https://www.paypal.com/sdk/js?client-id=${clientId}&currency=${currencyCode}&disable-funding=card,credit`
     var script = document.createElement('script')
     script.setAttribute('src', sdkUrl)
     document.head.appendChild(script)
