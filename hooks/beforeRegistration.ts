bbc8f020002969ae5e8476b6061802b7c355f8b1
Jamie <jamie@develodesign.co.uk>
2020-11-13 12:05:03 +0000

    Including latest ver. with changes

--- hooks/beforeRegistration.ts
+++ hooks/beforeRegistration.ts
@@ -1,13 +1,14 @@
+import { isServer } from '@vue-storefront/core/helpers'
 import { currentStoreView } from '@vue-storefront/core/lib/multistore'
 
-export function beforeRegistration({ Vue, config, store, isServer }) {
+export function beforeRegistration(config, store) {
   const VSF_PAYPAL_CODE = 'paypal_express'
 
-  if (!isServer && !config.paypal.addPaypalJsToHead) {
+  if (!isServer && config.hasOwnProperty('paypal') && !config.paypal.addPaypalJsToHead) {
     const storeView = currentStoreView()
     const { currencyCode } = storeView.i18n
-    const clientId = config.paypal.clientId
-    const sdkUrl = `https://www.paypal.com/sdk/js?client-id=${clientId}&currency=${currencyCode}&disable-funding=card,credit`
+    const clientId = config.paypal.hasOwnProperty('clientId') ? config.paypal.clientId : ''
+    const sdkUrl = `https://www.paypal.com/sdk/js?client-id=${clientId}&currency=${currencyCode}&disable-funding=card,credit,mybank,sofort`
     var script = document.createElement('script')
     script.setAttribute('src', sdkUrl)
     document.head.appendChild(script)
